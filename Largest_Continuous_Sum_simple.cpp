c728b4960b46ab3895bd889d4fbee3f83e6a5516
( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃ <hoholee12@naver.com>
2020-05-30 02:01:42 +0900

    Update Largest_Continuous_Sum_simple.cpp

--- Largest_Continuous_Sum_simple.cpp
+++ Largest_Continuous_Sum_simple.cpp
@@ -1,68 +1,29 @@
-#include<vector>
 #include<iostream>
+#include<vector>
 #include<climits>
-
-using vector = std::vector<int>;
-
 int main(){
-	vector hey;
 	int n;
-
+	std::vector<int> x;
 	std::cin >> n;
 	
 	int input;
-	int big = INT_MAX;
-	for (int i = 0; i < n; i++) { 
-		std::cin >> input; hey.push_back(input);
-		if (big > input) big = input;
+	for (int i = 0; i < n; i++) {
+		std::cin >> input;
+		x.push_back(input);
 	}
-	
-	int check = 0;	
-	int imm = big;
-	int loop = big;
-	int prevloop = loop << 1;
-	int flag = 1;
+
+	int result = INT_MIN;
+	int temp = 0;
 	for (int i = 0; i < n; i++){
-		loop += hey.at(i);
-		if (loop > prevloop){
-			check = i;
-			prevloop = loop;
-		}
-		if (imm < loop){
-			imm = loop;
-			flag = 1;
-		}
-		else if (flag == 1 || big > loop) {
-			flag = 0;
-			loop = big;
-		}
+		//append
+		temp += x.at(i);
+		//reset every big change
+		if (x.at(i) > temp) temp = x.at(i);
+		//get every big num
+		if (result < temp) result = temp;
 	}
 
-	vector left;
-	vector right;
-	big = INT_MIN;
-	input = 0;
-	for (int i = check - 1; i > -1; i--){
-		input += hey.at(i);
-		left.push_back(input);
-		if (big < input) big = input;
-	}
-	input = 0;
-	for (int i = check; i < n; i++){
-		input += hey.at(i);
-		right.push_back(input);
-		if (big < input) big = input;
-	}
-
-	input = 0;
-	for (int i = 0; i < left.size(); i++){
-		for (int j = 0; j < right.size(); j++){
-			input = left.at(i) + right.at(j);
-			if (big < input) big = input;
-		}
-	}
-
-	std::cout << big;
+	std::cout << result;
 
 	return 0;
 
