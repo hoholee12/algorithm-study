9fff958f526c9a5c6af24e3dad7ccac2b3597360
( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃ <hoholee12@naver.com>
2020-07-28 08:50:55 +0900

    custom buffersize!

--- MyStuff/ScoopSort.c
+++ MyStuff/ScoopSort.c
@@ -1,10 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void ScoopSort(int* arr, int size){
+int ScoopSort(int* arr, int size, int buf){
+
+	//subarr buffer size
+	buf += size;
 
 	int* bigarr = malloc(size * sizeof(int));
-	int* subarr = malloc(size * sizeof(int));
+	int* subarr = malloc(buf * sizeof(int));
 	//subarr init: O(N)
 	for (int i = 0; i < size; i++) subarr[i] = INT_MIN;
 
@@ -13,35 +16,39 @@ void ScoopSort(int* arr, int size){
 
 	//big loop(until all elements are scooped: O(count of 'diff bigger than size'))
 	int j = 0;
+	int loopcount = 0;
 	while (1){
 
-		//min check per section: O(N)
+		//min check per iteration: O(N)
 		int min = INT_MAX;
 		for (int i = 0; i < size; i++){
 			if (min > arr[i] && arr[i] > lastsize) min = arr[i];
 		}
 
-		//initval per section
+		//initval per iteration
 		int initval = min - 1;
 
 		//follow index: O(N)
 		for (int i = 0; i < size; i++){
 			int val = arr[i] - min;
-			if (val < size && val >= 0){
+			if (val < buf && val >= 0){
 				int x = 0; for (; subarr[val + x] > initval; x++);
 				subarr[val + x] = arr[i];
 			}
 		}
 
 		//scoop subarr to bigarr: O(N)
-		for (int i = 0; i < size; i++){
+		for (int i = 0; i < buf; i++){
 			if (subarr[i] > initval){
 				bigarr[j++] = subarr[i];
 				if (lastsize < subarr[i]) lastsize = subarr[i];
 			}
 		}
-		if (j >= size) break;
 
+		loopcount++;
+
+		//break on all scooped
+		if (j >= size) break;
 	}
 
 	//copy back: O(N)
@@ -51,14 +58,17 @@ void ScoopSort(int* arr, int size){
 	free(subarr);
 	free(bigarr);
 
+	return loopcount;
+
 }
 
 int main(){
-	int arr[5] = { 0, -101, -200, -200, 6 };
+	int arr[5] = { 1000, 123, 345, 123, 7 };
 
-	ScoopSort(arr, 5);
+	int result = ScoopSort(arr, 5, 1000);
 
 	for (int i = 0; i < 5; i++) printf("%d ", arr[i]);
+	printf("\nlooped %d time(s).\n", result);
 
 	return 0;
 }
