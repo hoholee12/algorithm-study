74ef7c19f2b2e3916b2e65583407c4430cb911e3
( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃ <hoholee12@naver.com>
2020-07-28 07:23:57 +0900

    Update ScoopSort.c

--- MyStuff/ScoopSort.c
+++ MyStuff/ScoopSort.c
@@ -3,35 +3,25 @@
 
 void ScoopSort(int* arr, int size){
 
-	//min check: O(N)
-	int minBackup = arr[0];
-	for (int i = 1; i < size; i++){
-		if (minBackup > arr[i]) minBackup = arr[i];
-	}
-	minBackup -= 1;
-	//min convert for zero or negative values: O(N)
-	for (int i = 0; i < size; i++) arr[i] -= minBackup;
-
-
 	int* bigarr = malloc(size * sizeof(int));
 	int* subarr = malloc(size * sizeof(int));
 	//subarr init: O(N)
 	for (int i = 0; i < size; i++) subarr[i] = INT_MIN;
 
 	//will always have biggest number of section
-	int lastsize = 0;
+	int lastsize = INT_MIN;
 
 	//big loop(until all elements are scooped: O(count of 'diff bigger than size'))
 	int j = 0;
 	while (1){
 
-		//min check per sub: O(N)
+		//min check per section: O(N)
 		int min = INT_MAX;
 		for (int i = 0; i < size; i++){
 			if (min > arr[i] && arr[i] > lastsize) min = arr[i];
 		}
 
-		//initval per sub
+		//initval per section
 		int initval = min - 1;
 
 		//follow index: O(N)
@@ -56,9 +46,6 @@ void ScoopSort(int* arr, int size){
 	//copy back: O(N)
 	for (int i = 0; i < size; i++) arr[i] = bigarr[i];
 
-
-	//min convert for zero or negative values: O(N)
-	for (int i = 0; i < size; i++) arr[i] += minBackup;
 
 	free(subarr);
 	free(bigarr);
