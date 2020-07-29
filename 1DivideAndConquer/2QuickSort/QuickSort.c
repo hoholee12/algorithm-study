3180dc8783929f29dab0cdfd3f287412881564a9
( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃ <hoholee12@naver.com>
2020-07-30 03:34:12 +0900

    Update QuickSort.c

--- 1DivideAndConquer/2QuickSort/QuickSort.c
+++ 1DivideAndConquer/2QuickSort/QuickSort.c
@@ -45,10 +45,10 @@ int Conquer(int* arr, int left, int right){
 	//swap numbers bigger than pivot to the right, smaller to the left
 	while(left_i < right + 1 && right_i > left){	//we need to count upto 'right'
 
-		while (arr[left_i] < arr[pivot]) left_i++;	//position left_i to next number bigger than pivot
-		while (arr[right_i] > arr[pivot]) right_i--;	//position right_i to next number smaller than pivot
+		while (arr[left_i] <= arr[pivot] && left_i <= right_i) left_i++;	//position left_i to next number bigger than pivot AND check interchange(end of the array)
+		while (arr[right_i] >= arr[pivot] && left_i <= right_i) right_i--;	//position right_i to next number smaller than pivot AND check interchange
 		
-		//swap pivot and break just after left and right interweave
+		//swap pivot and break just after left and right interchange
 		if (left_i > right_i){
 			swap(&arr[pivot], &arr[right_i]);
 			pivot = right_i;
@@ -76,21 +76,56 @@ void Divide(int* arr, int left, int right){
 	
 }
 
-int main(){ 
-#define ARRSIZE 1000
+int test2(){
+#define ARRSIZE 10000000
 	int* arr = malloc(ARRSIZE * sizeof(int));
 	srand(time(NULL));
 
 	for (int i = 0; i < ARRSIZE; i++){
-		arr[i] = rand();
+		arr[i] = rand() % ARRSIZE;
 
 	}
+
+	//printf("before: ");
+	//for (int i = 0; i < ARRSIZE; i++) printf("%d ", arr[i]);
+	//printf("\n");
 
 	time_t start = time(NULL);
 	Divide(arr, 0, ARRSIZE - 1);
 	time_t end = time(NULL);
 
-	for (int i = 0; i < ARRSIZE; i++) printf("%d ", arr[i]);
+	//printf("after: ");
+	//for (int i = 0; i < ARRSIZE; i++) printf("%d ", arr[i]);
+
 	printf("\ntook %d second(s).", end - start);
+
+	free(arr);
+	return 0;
+}
+
+int test1(){
+	
+	int arr[5] = { 5, 4, 3, 2, 1};
+	srand(time(NULL));
+
+	printf("before: ");
+	for (int i = 0; i < 5; i++) printf("%d ", arr[i]);
+	printf("\n");
+
+	time_t start = time(NULL);
+	Divide(arr, 0, 4);
+	time_t end = time(NULL);
+
+	printf("after: ");
+	for (int i = 0; i < 5; i++) printf("%d ", arr[i]);
+
+	printf("\ntook %d second(s).", end - start);
+
+	return 0;
+}
+
+int main(){
+	test2();
+
 	return 0;
 }
