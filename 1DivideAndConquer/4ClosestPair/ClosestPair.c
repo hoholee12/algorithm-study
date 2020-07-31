898e690d5f4938f658704a398aeb97532f2cd8fd
( ง ᵒ̌皿ᵒ̌)ง⁼³₌₃ <hoholee12@naver.com>
2020-07-31 20:12:56 +0900

    Update ClosestPair.c

--- 1DivideAndConquer/4ClosestPair/ClosestPair.c
+++ 1DivideAndConquer/4ClosestPair/ClosestPair.c
@@ -3,8 +3,8 @@
 
 typedef struct{
 	vector* arr;
-	int left;
-	int right;
+	int xleft;
+	int xright;
 } vecarr;
 
 double dist(pair a){
@@ -12,9 +12,13 @@ double dist(pair a){
 }
 
 //insertion sort
-pair threePairChooser(pair* temparr){
-	double distarr[3] = { dist(temparr[0]), dist(temparr[1]), dist(temparr[2]) };
-	for (int i = 1; i < 3; i++){
+pair nearestPairChooser(pair* temparr, int size){
+	double* distarr = malloc(size * sizeof(double));
+	for (int i = 0; i < size; i++){
+		distarr[i] = dist(temparr[i]);
+	}
+
+	for (int i = 1; i < size; i++){
 		double temp = distarr[i];
 		pair temp2 = temparr[i];
 		int j = i - 1;
@@ -33,48 +37,75 @@ pair threePairChooser(pair* temparr){
 		temparr[j + 1] = temp2;
 	}
 
+	free(distarr);
+
 	return temparr[0];
 
 
 }
 
 pair ClosestPair(vecarr S){
-	pair temp = { S.arr[S.left], S.arr[S.right] };
-	pair temp2 = { S.arr[S.left + 1], S.arr[S.right] };
-	pair temp3 = { S.arr[S.left], S.arr[S.right - 1] };
+	pair temp = { S.arr[S.xleft], S.arr[S.xright] };
+	pair temp2 = { S.arr[S.xleft + 1], S.arr[S.xright] };
+	pair temp3 = { S.arr[S.xleft], S.arr[S.xright - 1] };
 	pair temparr[3] = { temp, temp2, temp3 };
 	//return 2 or 3 pairs
-	switch (S.right - S.left + 1){
+	switch (S.xright - S.xleft + 1){
 	case 3:
-		return threePairChooser(temparr);
+		return nearestPairChooser(temparr, 3);
 	case 2:
 		return temp;
 	}
 
 	//cut S in half: SL, SR
 	//if odd num: SL = SR + 1
-	int half = (S.right - S.left) / 2 + S.left;
-	vecarr SL = { S.arr, S.left, half };
-	vecarr SR = { S.arr, half + 1, S.right };
+	int half = (S.xright - S.xleft) / 2 + S.xleft;
+	vecarr SL = { S.arr, S.xleft, half };
+	vecarr SR = { S.arr, half + 1, S.xright };
 
 	pair CPL = ClosestPair(SL);
 	pair CPR = ClosestPair(SR);
-	double d = min(dist(CPL), dist(CPR));
+	//immediate d calc for CPC
+	int d = (int)min(dist(CPL), dist(CPR));
 
-	//TODO
+	//make an array of pairs within +-d range for CPC
+	int scleft = half - d;
+	int scright = half + d;
+	if (scleft < S.xleft) scleft = S.xleft;
+	if (scright > S.xright) scright = S.xright;
+	vecarr SC = { S.arr, scleft, scright };
+	Divide(SC.arr, SC.xleft, SC.xright, yside);
+	double dmin = 0;
+	pair* tarr = malloc((SC.xright - SC.xleft + 1 + 2) * sizeof(pair));
 
-	return ;
+	int x = 0;
+	for (int i = SC.xleft; i <= SC.xright; i++){
+		if (SC.arr[i].x <= half){
+			for (int j = SC.xleft; j <= SC.xright; j++){
+				if (SC.arr[j].x > half){
+					pair tval = { SC.arr[i], SC.arr[j] };
+					tarr[x++] = tval;
+				}
+			}
+		}
+	}
+	
 
-
+	//throw in CPL CPR there as well without additional code
+	tarr[x++] = CPL;
+	tarr[x++] = CPR;
+	pair result = nearestPairChooser(tarr, (SC.xright - SC.xleft + 1 + 2));
+	free(tarr);
+	return result;
 }
 
 
 int main(){
-	vector temp[3] = { { 1, 5 }, { 2, 5 }, { 3, 2 }};
-	vecarr S = { temp, 0, 2 };
+	vector temp[5] = { { 1, 5 }, { 2, 5 }, { 3, 2 }, { 5, 4 }, {10, 1} };
+	vecarr S = { temp, 0, 4 };
 
 	//sort x side first
-	Divide(S.arr, S.left, S.right, xside);
+	Divide(S.arr, S.xleft, S.xright, xside);
 	
 	//for (int i = 0; i < 5; i++) printf("(%d, %d) ", S.arr[i].x, S.arr[i].y);
 	
